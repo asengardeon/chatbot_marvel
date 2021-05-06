d915ea69b69e8488d243dd699c4322a2b24f6ef6
Lucas Amaral <lucas.amaral.dw@gmail.com>
2021-05-06 08:17:45 -0300

    Usando entidades para determinar qual o heroi que o usuário quer conhecer sobre

--- rasa-nlu/actions/actions.py
+++ rasa-nlu/actions/actions.py
@@ -7,21 +7,26 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.events import SlotSet
+from rasa_sdk.executor import CollectingDispatcher
+#from marvel import Marvel
+
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "action_character_description"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        PUBLIC_KEY = "8e015296ba3e131887a0d5d2df0f1a9f"
+        PRIVATE_KEY = "5dc31df476a489fc560bcbdd67646a4feed203ce"
+        #m = Marvel(PUBLIC_KEY, PRIVATE_KEY)
+        char_name = next(tracker.get_latest_entity_values("character_name"), None)
+        #char = m.characters.all(nameStartsWith=char_name)
+        description = char_name #char['data']['results'][0]['description']
+        dispatcher.utter_message(text="Sim! conheço o " + char_name)
+        return []
