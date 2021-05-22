96ed47d9603efa188fc647f0a6ab5016bb10bfb6
Cristopher Braatz Cardoso <crisbraatz@icloud.com>
2021-05-22 01:12:48 -0300

    Incluido query para salvar voto (desativado)

--- rasa_actions_server/actions/actions.py
+++ rasa_actions_server/actions/actions.py
@@ -17,6 +17,8 @@ from .marvel_requests import marvel_request
 from rasa_sdk.events import AllSlotsReset
 from rasa_sdk.events import SlotSet
 
+# from pymongo import MongoClient
+
 class ActionGreet(Action):
 
      def name(self) -> Text:
@@ -232,5 +234,28 @@ class ActionSaveVote(Action):
         vote = tracker.get_slot('vote')
 
         dispatcher.utter_message(text=f"Obrigado por votar, seu voto computado foi {vote}")
+        # salvar_votacao(vote)
         return []
 
+def salvar_votacao(votacao: int):
+    mongo_host = '127.0.0.1'
+    mongo_port = '27017'
+    mongo_user = 'rasa'
+    mongo_password = 'rasa'
+    mongo_args = '?authSource=admin&ssl=false'
+    mongo_db_name = 'rasa'
+    mongo_collection = 'conversations'
+
+    mongo_connection_string = f'mongodb://{mongo_user}:{mongo_password}@{mongo_host}:{mongo_port}/{mongo_args}'
+
+    mongo_client = MongoClient(mongo_connection_string)
+
+    db = mongo_client[mongo_db_name]
+
+    target_collection = db[mongo_collection]
+
+    id = target_collection.find_one({'projection': {'_id': 1}}).sort({'_id': -1}).limit(1)
+
+    target_collection.update({'_id': id}, {"$set": {'events.event': 'votacao', 'events.votacao': votacao}}
+
+    mongo_client.close()
